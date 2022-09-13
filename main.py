f7c24df27988be89b4eb9dee05ecfbb08e92356f
Roberto Genchi <rob.genchi@gmail.com>
2022-09-13 18:26:33 +0200

    script completed!

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from pedalboard import Pedalboard, Reverb, load_plugin
 from pedalboard.io import AudioFile
+import time
 
-print("Import completato con successo")
+start = time.time()
+
+# Let's upload an existing third-party plugin (VST3)
+vst = load_plugin("/Library/Audio/Plug-Ins/VST3/Auburn Sounds Graillon 2.vst3")
+
+# Now we print the parameters possessed by the plugins
+print(vst.parameters.keys())
+
+# We change the 'pitch_shift_st' parameter so that we have a voice gender transformation. Check it out!
+vst.pitch_shift_st = 3.84
+
+# Let's add a stock reverb plugin to the effects chain
+board = Pedalboard([vst, Reverb()])
+
+# Let's open an audio file and apply our board
+with AudioFile('media/input_file.wav', 'r') as f:
+    audio = f.read(f.frames)
+    samplerate = f.samplerate
+
+effected = board(audio, samplerate)
+
+# Write the audio back as a wav file:
+with AudioFile('media/output_file.wav', 'w', samplerate, effected.shape[0]) as f:
+  f.write(effected)
+
+end = time.time()
+total_time = end - start
+print("\nTime elapsed: " + str(total_time) + " seconds")
